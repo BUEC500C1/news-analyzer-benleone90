39eda1d9bbf4530763207c7998541e9deba127be
Ben Leone <benleone90@gmail.com>
2021-02-14 12:57:12 -0500

    Updated package imports

--- nlp_analysis.py
+++ nlp_analysis.py
@@ -1,12 +1,10 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import Api, Resource
 from google.cloud import language_v1
 
 app = Flask(__name__)
 api = Api(app)
 client = language_v1.LanguageServiceClient()
-
-# text = u"Hello, World!"  # Unicode string
 
 
 class analyzeSent(Resource):
