11e78d6519ad05c660183e5576289dea264bf979
Ben Leone <benleone90@gmail.com>
2021-02-20 13:01:27 -0500

    Update NLP analysis and testing for Flask-only methods

--- nlp_analysis/nlp_analysis.py
+++ nlp_analysis/nlp_analysis.py
@@ -1,22 +1,18 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
 from google.cloud import language_v1
 
 app = Flask(__name__)
-api = Api(app)
 client = language_v1.LanguageServiceClient()
 
 
-class analyzeSent(Resource):
-    def get(self, text):
-        document = language_v1.Document(
-            content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-        sentiment = client.analyze_sentiment(
-            request={'document': document}).document_sentiment
-        return {'text': text, 'score': sentiment.score, 'magnitude': sentiment.magnitude}
+@app.route("/nlp/<text>")
+def analyzeSentiment(text=None):
+    document = language_v1.Document(
+        content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+    sentiment = client.analyze_sentiment(
+        request={'document': document}).document_sentiment
+    return {'text': text, 'score': sentiment.score, 'magnitude': sentiment.magnitude}
 
-
-api.add_resource(analyzeSent, "/nlp/<string:text>")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- nlp_analysis/test_nlp.py
+++ nlp_analysis/test_nlp.py
@@ -1,10 +1,10 @@
 import pytest
 import requests
-from nlp_analysis import analyzeSent
+from nlp_analysis import analyzeSentiment
 
 # Check on how to deply envvar for GitHub Actions with NLP
 
 
 def test_status():
-    response = requests.get('http://127.0.0.1:5000/nlp/bitcoin')
+    response = requests.get('http://127.0.0.1:5000/nlp/Hello World!')
     assert response.status_code == 200
