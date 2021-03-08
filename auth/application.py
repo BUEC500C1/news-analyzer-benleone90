8e063e50215b299e0c4736405508aa73c1745d0e
Ben Leone <benleone90@gmail.com>
2021-03-07 19:38:22 -0500

    Local login working

--- auth/application.py
+++ auth/application.py
@@ -25,17 +25,17 @@ def login():
         password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(
-            'SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+            'SELECT * FROM auth WHERE username = % s AND password = % s', (username, password, ))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
             msg = 'Logged in successfully'
-            return render_template('auth.html', msg=msg)
+            return render_template('index.html', msg=msg)
         else:
             msg = 'Incorrect username or password'
-    return render_template('#', msg=msg)
+    return render_template('auth.html', msg=msg)
 
 
 @application.route('/logout')
@@ -43,7 +43,33 @@ def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
-    return redirect(url_for('auth'))
+    return redirect(url_for('login'))
+
+
+@application.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT * FROM auth WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute(
+                'INSERT INTO auth VALUES (NULL, % s, % s)', (username, password, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg=msg)
 
 
 if __name__ == '__main__':
