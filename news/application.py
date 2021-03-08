d51fba61379db8acb4e1d4c21f5b3261b377c90c
Ben Leone <benleone90@gmail.com>
2021-03-07 21:19:35 -0500

    Updated application for use with form data

--- news/application.py
+++ news/application.py
@@ -1,25 +1,26 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, jsonify
 from newsapi import NewsApiClient
 import os
 
 # template_dir = os.path.abspath('./')
-app = Flask(__name__)
+application = Flask(__name__)
 news_key = os.environ.get("NEWS_KEY")
 newsapi = NewsApiClient(api_key=news_key)
 
 
-@app.route('/')
+@application.route('/')
 def index():
-    return render_template("news_injest.html")
+    return render_template("news.html")
 
 
-@app.route('/news/<string:query>', methods=['GET'])
-def all_articles(query):
-    all_articles = newsapi.get_everything(
+@application.route('/news', methods=['POST'])
+def news():
+    query = request.form.get('query')
+    results = newsapi.get_everything(
         q=query, language='en', sort_by='relevancy')
     #app.logger.info('Test message')
-    return render_template("news_injest.html", results=all_articles)
+    return render_template("news.html", results=results)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
--- nlp/application.py
+++ nlp/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, render_template, jsonify
 from google.cloud import language_v1
 import os
 
@@ -9,20 +9,20 @@ client = language_v1.LanguageServiceClient()
 
 @app.route("/")
 def index():
-    return render_template("nlp_analysis.html")
+    return render_template("nlp.html")
 
 
-@app.route("/nlp/<text>")
-def analyzeSentiment(text=None):
+@app.route("/nlp", methods=['GET', 'POST'])
+def nlp():
+    text = request.form.get('content')
     document = language_v1.Document(
         content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
     sentiment = client.analyze_sentiment(
         request={'document': document}).document_sentiment
     # app.logger.info('Test message')
-    content = {'text': text, 'score': sentiment.score,
-               'magnitude': sentiment.magnitude}
-    jsonify(content)
-    return render_template("nlp_analysis", content=content)
+    response = {'text': text, 'score': sentiment.score,
+                'magnitude': sentiment.magnitude}
+    return render_template("nlp.html", response=response)
 
 
 if __name__ == '__main__':
