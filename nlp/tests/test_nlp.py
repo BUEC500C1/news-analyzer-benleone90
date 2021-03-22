229270a784c9338726cd96906a846aa48fcfbee2
Ben Leone <benleone90@gmail.com>
2021-03-22 14:42:09 -0400

    Updated test

--- nlp/tests/test_nlp.py
+++ nlp/tests/test_nlp.py
@@ -6,6 +6,11 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 
-def test_status():
-    response = requests.get('http://127.0.0.1:5000/nlp/sentiment/Hello World!')
-    assert response.status_code == 200
+# def test_status():
+#     response = requests.get('http://127.0.0.1:5000/nlp/sentiment/Hello World!')
+#     assert response.status_code == 200
+
+
+def test_sentiment():
+    response = analyzeSentiment('Hello world!')
+    assert type(response) is dict
